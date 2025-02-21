14e8551e4acd31234c3c72f8cebe43911b000481
jramosss <jramostod@gmail.com>
2025-02-21 19:22:10 -0300

    removes mocks

--- notifier.py
+++ notifier.py
@@ -14,8 +14,11 @@ RECIPIENTS = [
     TEST_EMAIL
 ]
 
-smtp_server = '0.0.0.0'
-smtp_port = 1025
+if not os.getenv('SMTP_SERVER') or not os.getenv('SMTP_PORT'):
+    raise Exception('SMTP_SERVER and SMTP_PORT are required')
+
+smtp_server = os.getenv('SMTP_SERVER')
+smtp_port = os.getenv('SMTP_PORT')
 
 def send_email(to: list[str], events: list[Event]):
     msg = MIMEMultipart()
@@ -26,8 +29,6 @@ def send_email(to: list[str], events: list[Event]):
     msg['Content-Type'] = 'text/html'
     html = generate_html_for_events(events)
     msg.attach(MIMEText(html, 'html'))
-    with open('mocks/generated.html', 'w') as file:
-        file.write(html)
 
     with smtplib.SMTP(smtp_server, smtp_port) as server:
         server.sendmail(me, to, msg.as_string())
